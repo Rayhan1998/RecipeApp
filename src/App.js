a98d8c31e3a95046267128d5415e4600e509f842
Rayhan1998 <miahrayhan01@gmail.com>
2020-12-05 16:09:01 -0500

    feat:installed router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hell</div>;
 }
 
 export default App;
