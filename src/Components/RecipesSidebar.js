b9148675a37322326606ff1c494aae0a50ef79a2
Rayhan1998 <miahrayhan01@gmail.com>
2020-12-09 01:31:52 -0500

    feat: added git repo link

--- src/Components/RecipesSidebar.js
+++ src/Components/RecipesSidebar.js
@@ -36,6 +36,20 @@ export default function RecipesSidebar({
           />
         );
       })}
+
+      <FooterWrapper>
+        <h1 style={{ color: "orange", marginRight: "15px" }}>Project Repo</h1>
+        <a
+          className="github-link"
+          href="https://github.com/Rayhan1998/RecipeApp"
+          target="_blank"
+        >
+          <i
+            className="fab fa-github"
+            style={{ fontSize: "60px", color: "grey" }}
+          ></i>
+        </a>
+      </FooterWrapper>
     </RecipeSidebarWrapper>
   );
 }
@@ -49,5 +63,22 @@ const RecipeSidebarWrapper = styled.div`
     width: 100%;
     overflow-x: scroll;
     display: flex;
+  }
+`;
+
+const FooterWrapper = styled.div`
+  height: 150px;
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  .github-link {
+    text-decoration: none;
+    color: white;
+  }
+
+  @media (max-width: 1150px) {
+    display: none;
   }
 `;
